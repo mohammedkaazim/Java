a2378b588e8297e0048cedce6152fdc0f8918f07
mohammedkaazim <umohammedkaazim@gmail.com>
2025-06-05 17:15:56 +0530

    Simple Calculator

---
+++ JAVA/Day2-Conditional Statements/SimpleCalculator.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        double a = sc.nextDouble();
+        System.out.print("Enter second number: ");
+        double b = sc.nextDouble();
+
+        System.out.println("Choose operation (+, -, *, /): ");
+        char op = sc.next().charAt(0);
+
+        switch (op) {
+            case '+':
+                System.out.println("Sum = " + (a + b));
+                break;
+            case '-':
+                System.out.println("Difference = " + (a - b));
+                break;
+            case '*':
+                System.out.println("Product = " + (a * b));
+                break;
+            case '/':
+                if (b != 0)
+                    System.out.println("Quotient = " + (a / b));
+                else
+                    System.out.println("Cannot divide by zero!");
+                break;
+            default:
+                System.out.println("Invalid operator!");
+        }
+    }
+}
