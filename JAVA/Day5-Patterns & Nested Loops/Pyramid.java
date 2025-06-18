dd514607c3be14c1e4e7d866e9e5551a16c2bc5f
mohammedkaazim <umohammedkaazim@gmail.com>
2025-06-18 09:59:25 +0530

    Prints Pyramid Patterns

---
+++ JAVA/Day5-Patterns & Nested Loops/Pyramid.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Pyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int space = 1; space <= n - i; space++) {
+                System.out.print(" ");
+            }
+            for (int star = 1; star <= (2 * i - 1); star++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
